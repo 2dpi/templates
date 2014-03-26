404d190831ff5bbfde0fb7b5834f3a2aee1971ab
2dpi <info@2dpi.co.za>
2014-03-26 18:36:28 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".toggle_show").hide();
 	$(".toggle").click(function () {
-		$(this.target).closest(".toggle_hide").toggle();
-		$(this.target).closest(".toggle_show").toggle();
+		$.closest(".toggle_hide").toggle();
+		$.closest(".toggle_show").toggle();
 	});
 });
