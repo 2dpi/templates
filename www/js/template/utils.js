e440142c855e471f4a1b7a6bcc447c0757e5cab4
2dpi <info@2dpi.co.za>
2014-03-26 17:52:14 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$(".toggle_show").hide();
 	$(".toggle").click(function () {
-		$(this).nextAll(".toggle_hide").first().toggle();
-		$(this).nextAll(".toggle_show").first().toggle();
+		//$(this).nextAll(".toggle_hide").first().toggle();
+		//$(this).nextAll(".toggle_show").first().toggle();
+		$('.toggle_hide').nextAll('matchSelector:first').toggle();
+		$('.toggle_show').nextAll('matchSelector:first').toggle();
 	});
 });
