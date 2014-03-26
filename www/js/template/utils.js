f6f93f6b4e9f7bba1f5341f6cfd66d8fff7fd799
2dpi <info@2dpi.co.za>
2014-03-26 18:46:11 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,15 +1,11 @@
 var toggleHide = $( ".toggle_hide" );
 var toggleShow = $( ".toggle_show" );
-$( document ).on( "click", function( event ) {
-	$( event.target ).closest( toggleHide ).toggle();
-	$( event.target ).closest( toggleShow ).toggle();
-
-});
 $(document).ready(function(){
 	$(".toggle_show").hide();
-	// $(".toggle_show").hide();
-	// $(".toggle").click(function () {
+	$(".toggle").click(function (event) {
+		$( event.target ).closest( toggleHide ).toggle();
+		$( event.target ).closest( toggleShow ).toggle();
 	// 	$.closest(".toggle_hide").toggle();
 	// 	$.closest(".toggle_show").toggle();
-	// });
+	});
 });
