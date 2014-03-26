75682d7c707443615ef96277e9048f6989189a01
2dpi <info@2dpi.co.za>
2014-03-26 18:52:46 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
+	$(".toggle_show").hide();
+});
 var toggleHide = $( ".toggle_hide" );
 var toggleShow = $( ".toggle_show" );
-	$(".toggle_show").hide();
-	$(".toggle").click(function (event) {
-		$( event.target ).closest( toggleHide ).toggle();
-		$( event.target ).closest( toggleShow ).toggle();
-	// 	$.closest(".toggle_hide").toggle();
-	// 	$.closest(".toggle_show").toggle();
-	});
+$( document ).on( "click", function( event ) {
+	$( event.target ).closest( toggleHide ).toggle();
+	$( event.target ).closest( toggleShow ).toggle();
 });
