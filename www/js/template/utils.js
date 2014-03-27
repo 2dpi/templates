caaa621eb2b18cc68881bc85c1d0e7050939f061
2dpi <info@2dpi.co.za>
2014-03-27 07:25:55 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$(".toggle_show").hide();
 	$(".toggle").click(function () {
-		$(this).nextUntil(".toggle_hide").toggle();
-		$(this).nextUntil(".toggle_show").toggle();
+		$(this).parent().next('.toggle_hide').slideDown('slow');
+		$(this).parent().next('.toggle_show').slideDown('slow');
+		// $(this).nextUntil(".toggle_hide").toggle();
+		// $(this).nextUntil(".toggle_show").toggle();
 	});
 });
