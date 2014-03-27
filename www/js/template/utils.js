09b7ab52bd12bb73cd92b78c4a869da2e788927b
2dpi <info@2dpi.co.za>
2014-03-27 08:44:15 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$(".toggle_show").hide();
 	$(".toggle").click(function () {
-		$(this).parent().parent().nextUntil('.toggle_show,.toggle_hide').toggle();
-		//$(this).parent().parent().nextUntil('.toggle_hide').toggle();
+		$(this).parent().parent().nextUntil('.toggle_hide').toggle();
+		$(this).parent().parent().nextUntil('.toggle_show').toggle();
 		// $(this).nextUntil(".toggle_hide").toggle();
 		// $(this).nextUntil(".toggle_show").toggle();
 	});
