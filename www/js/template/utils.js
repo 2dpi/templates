c2f0b8cbb525fd2599f56860540cdefadc91997c
2dpi <info@2dpi.co.za>
2014-03-27 05:51:00 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".toggle_show").hide();
 	$(".toggle").click(function () {
-		$(this).siblings(".toggle_hide").toggle();
-		$(this).siblings(".toggle_show").toggle();
+		$(this).siblings.next(".toggle_hide").toggle();
+		$(this).siblings.next(".toggle_show").toggle();
 	});
 });
