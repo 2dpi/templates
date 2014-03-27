573002f2697359d45fef4055e47749a8119c98f0
2dpi <info@2dpi.co.za>
2014-03-27 06:54:14 +0200

    adding pages

--- www/js/template/utils.js
+++ www/js/template/utils.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 	$(".toggle_show").hide();
-	$(".toggle").click(function () {
-		//$(this).siblings.next(".toggle_hide").toggle();
-		//$(this).siblings.next(".toggle_show").toggle();
+	// $(".toggle").click(function () {
+	// 	//$(this).siblings.next(".toggle_hide").toggle();
+	// 	//$(this).siblings.next(".toggle_show").toggle();
 
-		var thisIs = $(this).index();
-        $('.toggle_hide').eq(thisIs).toggle();
-        $('.toggle_show').eq(thisIs).toggle();
+	// 	var thisIs = $(this).index();
+ //        $('.toggle_hide').eq(thisIs).toggle();
+ //        $('.toggle_show').eq(thisIs).toggle();
+	// });
+
+	$('.toggle').each( function(e){
+        $(this).bind('click', function(e){ var thisIs = $(this).index(); 
+    	$('.toggle_hide').eq(thisIs).slideToggle(1000); });
 	});
 });
